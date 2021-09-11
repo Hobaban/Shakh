09973a7c2fb88695a1ff054c2e51f7d8c51e2475
xenups <xebups@outlook.com>
2021-09-11 16:07:49 +0430

    product tab added to admin panel

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Product, ProductImage
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline, ]
+    list_display = ('id', 'name', 'created_date', 'updated_date')
+    list_filter = ('name', 'created_date', 'updated_date')
+
+
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='product_images', on_delete=models.CASCADE, blank=True)
+    image = models.ImageField(upload_to="media", blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.name
