6b92bbc2f01ff84490a1622c3fbf96dcec6ccfb5
xenups <xebups@outlook.com>
2021-09-12 16:44:47 +0430

    upload ProductImage added

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from product.views import ProductDetailView, ProductCreateListView
+from product.views import ProductDetailView, ProductCreateListView, ProductImage
 
 urlpatterns = [
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('products/', ProductCreateListView.as_view(), name='product-list')
+    path('products/', ProductCreateListView.as_view(), name='product-list'),
+    path('products/images/', ProductImage.as_view(), name='product-images')
 ]
