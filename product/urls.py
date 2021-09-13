344d6dbe83be96abf19b9623dda659dfb5c389a3
xenups <xebups@outlook.com>
2021-09-14 01:08:02 +0430

    URL path using dispatcher to assign different REST functions to one route

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from product.views import ProductDetailView, ProductCreateListView, ProductImage
+
+from product import views
+from util.url import method_dispatch
 
 urlpatterns = [
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('products/', ProductCreateListView.as_view(), name='product-list'),
-    path('products/images/', ProductImage.as_view(), name='product-images')
+    path('products/', method_dispatch(POST=views.add_product_controller, GET=views.get_products_controller),
+         name='product-list'),
+    path('products/<int:pk>/',
+         method_dispatch(GET=views.get_product_controller, PUT=views.update_product_controller,
+                         DELETE=views.remove_product_controller),
+         name='product-detail'),
+    path('products/images/', method_dispatch(POST=views.add_product_image_controller), name='product-images')
 ]
