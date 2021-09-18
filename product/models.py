d42bc633a9fe59824a5b9f9e6d31b90107031fba
xenups <xenups@outlook.com>
2021-09-19 02:31:51 +0430

    Reviews admin panel and models added

--- product/models.py
+++ product/models.py
@@ -1,4 +1,7 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+from account.models import User
 
 
 class Product(models.Model):
@@ -8,6 +11,30 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=250)
+    rate = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    context = models.CharField(max_length=1024)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviewers')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_reviews', blank=True,
+                                null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ReviewImage(models.Model):
+    review = models.ForeignKey(Review, related_name='review_images', on_delete=models.CASCADE, blank=True)
+    image = models.ImageField(upload_to="media", blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.review.title
 
 
 class ProductImage(models.Model):
